e9e9bddff8e7640d31c98084da53a14b6433e26b
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-12-05 01:16:53 +0800

    add(input-page): add page for inputting necessary data for simulation

---
+++ pages/fam/lru.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button class="btn btn-secondary" @click="updateShow">
+      Show/Hide
+    </button>
+    <h1 v-if="show" class="text-5xl">Greet</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const name = useState('name', () => '');
+const show = useState('show', () => false);
+
+const updateShow = () => {
+  show.value = !show.value;
+}
+</script>
