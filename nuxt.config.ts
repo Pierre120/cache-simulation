a0154cc095b5f611f3abc091cbe97399ce9222e2
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-12-04 19:20:47 +0800

    style(ui): setup tailwindcss, color-mode and daisyui for ui style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+
+  colorMode: {
+    preference: 'night', // default theme
+    dataValue: 'theme', // activate data-theme in <html> tag
+    classSuffix: '',
+  },
+
   // For Vercel deployment
   nitro: {
     preset: 'vercel-edge',
